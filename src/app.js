d874a85848fd5681fdc9a1001e263d231df51f90
Michael Blake Donaldson <Blake18465@gmail.com>
2026-02-15 11:25:00 -0500

    Fixing port problem

--- src/app.js
+++ src/app.js
@@ -11,6 +11,28 @@ const app = express();
 app.use(express.json());
 app.set('trust proxy', 1);
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  const allowedOrigins = new Set([
+    'http://127.0.0.1:5500',
+    'http://localhost:5500'
+  ]);
+
+  if (origin && allowedOrigins.has(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+    return;
+  }
+
+  next();
+});
+
 const urlRepository = new UrlRepository(db);
 const cache = new LruCache(config.cacheMaxItems, config.cacheTtlMs);
 const urlService = new UrlService({
